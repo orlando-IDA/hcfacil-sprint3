3dc7d04b4e212a2ee320debb537b2904320146f4
ggabmartins <gabriellm1805@gmail.com>
2025-09-23 09:07:56 -0300

    -Array com perguntas frequentes.

--- src/pages/FaqPage.tsx
+++ src/pages/FaqPage.tsx
@@ -1,5 +1,32 @@
-// src/pages/FaqPage.tsx
+
 import React from 'react';
+
+ const faqData = [
+  {
+    question: 'Onde estão os vídeos?',
+    answer: 'Basta acessar a página principal (clique na nossa logo no topo da página) e clicar na aba de vídeos.'
+  },
+  {
+    question: 'Como faço meu cadastro?',
+    answer: 'Acesse o Portal do Paciente do HC ou para maiores explicações, clique na nossa logo e vá em cadastro.'
+  },
+  {
+    question: 'Quem criou essa plataforma?',
+    answer: 'Clique na aba de "INTEGRANTES" no menu no topo da página para conhecer a equipe.'
+  },
+  {
+    question: 'Quais são os contatos da HC?',
+    answer: 'Clique na aba de "CONTATO" no menu no topo da página para ver os telefones e e-mail.'
+  },
+  {
+    question: 'Onde vejo minhas consultas marcadas?',
+    answer: 'Acesse a aba de "Minhas Agendas" no portal oficial.'
+  },
+  {
+    question: 'Como mudar minha senha?',
+    answer: 'Clique na nossa logo e acesse a aba de "Esqueci a minha senha" no portal oficial.'
+  }
+];
 
 const FaqPage = () => {
   return (
@@ -10,7 +37,7 @@ const FaqPage = () => {
         </h1>
         
         <div className="bg-white p-6 rounded-lg shadow-md">
-          {/* Aqui serão renderizadas as perguntas e respostas */}
+          {/* As perguntas e respostas serão renderizadas no próximo commit */}
         </div>
       </div>
     </main>
